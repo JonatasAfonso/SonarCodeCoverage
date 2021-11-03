285da66a23807ac65e52b2cd54f0523493200100
Jônatas Afonso Oliveira Neto <jonatas.oliveira.neto@fidelidade.pt>
2021-11-03 16:55:29 +0000

    Write-host "Ok ate 23"

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -24,14 +24,21 @@ Write-host "Ok ate 23"
 $version = Invoke-Gitversion
 $assemblyVer = $version.assemblyVersion 
 
+Write-host "Ok ate 27"
 $branch = git branch --show-current
+Write-host "Ok ate 29"
 Write-Host "branch is $branch"
 
 dotnet tool restore
+Write-host "Ok ate 33"
 dotnet tool run dotnet-sonarscanner begin /k:"JonatasAfonso_SonarCodeCoverage" /v:"$assemblyVer" /o:"alkampfergit-github" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
+Write-host "Ok ate 35"
 
 dotnet restore src
+Write-host "Ok ate 38"
 dotnet build src --configuration release
+Write-host "Ok ate 40"
 dotnet test "./ProjetoTests/ProjetoTests.csproj" --collect:"XPlat Code Coverage" --results-directory TestResults/ --logger "trx;LogFileName=unittests.trx" --no-build --no-restore --configuration release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
-         
+Write-host "Ok ate 42"
 dotnet tool run dotnet-sonarscanner end /d:sonar.login="$sonarSecret"
+Write-host "Ok ate 44"
