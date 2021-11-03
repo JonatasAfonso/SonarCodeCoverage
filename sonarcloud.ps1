a64ab6b17af5e1752546e53aecb8abfda46c5d1b
Jônatas Afonso Oliveira Neto <jonatas.oliveira.neto@fidelidade.pt>
2021-11-03 16:39:47 +0000

    Correcao script

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -25,7 +25,7 @@ Write-Host "branch is $branch"
 
 dotnet tool restore
 # dotnet tool run dotnet-sonarscanner begin /k:"alkampfergit_DotNetCoreCryptography" /v:"$assemblyVer" /o:"alkampfergit-github" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
-dotnet tool run dotnet-sonarscanner begin /k:"JonatasAfonso_SonarCodeCoverage" /o:"rumosamazonia" /d:sonar.login="${{ secrets.SONAR_TOKEN }}" /d:sonar.host.url="https://sonarcloud.io /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
+dotnet tool run dotnet-sonarscanner begin /k:"JonatasAfonso_SonarCodeCoverage" /o:"rumosamazonia" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
 
 dotnet restore src
 dotnet build src --configuration release
